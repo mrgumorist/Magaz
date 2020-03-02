234d38da0b93d4f55168ee21cea3c6e1dde4ed5d
Smirnov Andrii <Smirnov Andrii@A24PC8>
2020-03-02 17:07:04 +0200

    jghg

--- Magazine/AdminFolder/Reports.xaml.cs
+++ Magazine/AdminFolder/Reports.xaml.cs
@@ -34,55 +34,55 @@ namespace Magazine.AdminFolder
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(D1.SelectedDate.Value.ToLongDateString()==D2.SelectedDate.Value.ToLongDateString())
-            {
+            MessageBox.Show("Починаємо пошук! Зачекайте.");
+            if (D1.SelectedDate.Value.ToLongDateString() == D2.SelectedDate.Value.ToLongDateString()) {
+
                 
-                string WEBSERVICE_URL1 = StaticHelper.URL + $@"api/Apii/GetSalesByDate";
-
-                var webRequest1 = System.Net.WebRequest.Create(WEBSERVICE_URL1);
-                if (webRequest1 != null)
-                {
-                    webRequest1.Method = "POST";
-                    webRequest1.Timeout = 12000;
-                    webRequest1.ContentType = "application/json";
-                    webRequest1.Headers.Add("Safety", "Safety");
-                    using (var streamWriter2 = new StreamWriter(webRequest1.GetRequestStream()))
-                    {
-                        string @str = "";
-
-                        str += JsonConvert.SerializeObject(D1.SelectedDate.Value);
-                        streamWriter2.Write(str);
-                    }
-                    //webRequest1.Headers.Add("SpecialCode", Code.Text);
-                    try
-                    {
-                        using (System.IO.Stream s1 = webRequest1.GetResponse().GetResponseStream())
-                        {
-                            using (System.IO.StreamReader sr1 = new System.IO.StreamReader(s1))
-                            {
-                                try
-                                {
-                                    var jsonResponse1 = sr1.ReadToEnd();
-                                    double kek = double.Parse(jsonResponse1);
-                                    MessageBox.Show(kek.ToString());
-                                }
-                                catch
-                                {
-                                    MessageBox.Show("Error");
-                                }
-                            }
-                        }
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Error");
-                    }
-                    }
             }
             else
             {
-
+                
             }
+            
+               string WEBSERVICE_URL1 = StaticHelper.URL + $@"api/Apii/GetSalesByDate";
+
+               var webRequest1 = System.Net.WebRequest.Create(WEBSERVICE_URL1);
+               if (webRequest1 != null)
+               {
+                   webRequest1.Method = "POST";
+                   webRequest1.Timeout = 12000;
+                   webRequest1.ContentType = "application/json";
+                   webRequest1.Headers.Add("Safety", "Safety");
+                   using (var streamWriter2 = new StreamWriter(webRequest1.GetRequestStream()))
+                   {
+                      // ;
+                       streamWriter2.Write(JsonConvert.SerializeObject(D1.SelectedDate.Value));
+                   }
+                   try
+                   {
+                       using (System.IO.Stream s1 = webRequest1.GetResponse().GetResponseStream())
+                       {
+                           using (System.IO.StreamReader sr1 = new System.IO.StreamReader(s1))
+                           {
+                               try
+                               {
+                                   var jsonResponse1 = sr1.ReadToEnd();
+                                   double kek = double.Parse(jsonResponse1);
+                                   MessageBox.Show(kek.ToString());
+                               }
+                               catch
+                               {
+                                   MessageBox.Show("Error");
+                               }
+                           }
+                       }
+                   }
+                   catch
+                   {
+                       MessageBox.Show("Error");
+                   }
+                   }
+       
         }
     }
 }
