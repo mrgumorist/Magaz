bf2392232bced88c510c56004bd8fb87953b209a
mrgumorist <mrgumorist@gmail.com>
2020-03-27 00:40:38 +0200

    f

--- Magazine/AdminFolder/Calculator.xaml.cs
+++ Magazine/AdminFolder/Calculator.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Magazine.ModelsDto;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,9 +22,108 @@ namespace Magazine.AdminFolder
     /// </summary>
     public partial class Calculator : Page
     {
+        public List<CalcIn> Prihods;
+        public List<CalcOut> Rozhods;
+        public List<Spisannya> Spisannya;
+        public List<SpisannyaOnAnotherMarket> SpisannyaOnAnotherMarket;
+        public List<Lesia> LesiaS;
+        public List<Lena> LenaS;
         public Calculator()
         {
             InitializeComponent();
+            Update();
+        }
+        private void Update()
+        {
+            string WEBSERVICE_URL1 = StaticHelper.URL + @"api/Apii/GetAllInAndOuts";
+
+            var webRequest1 = System.Net.WebRequest.Create(WEBSERVICE_URL1);
+            if (webRequest1 != null)
+            {
+                webRequest1.Method = "GET";
+                webRequest1.Timeout = 12000;
+                webRequest1.ContentType = "application/json";
+                webRequest1.Headers.Add("Safety", "Safety");
+                //webRequest.Headers.Add("StoreData", JsonConvert.SerializeObject(store));
+                using (System.IO.Stream s1 = webRequest1.GetResponse().GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr1 = new System.IO.StreamReader(s1))
+                    {
+                        try
+                        {
+                            var jsonResponse1 = sr1.ReadToEnd();
+                            InAndOutsDto inAndOutsDto = JsonConvert.DeserializeObject<InAndOutsDto>(jsonResponse1);
+                            Prihods = inAndOutsDto.Prihods;
+                            Rozhods = inAndOutsDto.Rozhods;
+                            LenaS = inAndOutsDto.LenaS;
+                            LesiaS = inAndOutsDto.LesiaS;
+                            Spisannya = inAndOutsDto.Spisannya;
+                            SpisannyaOnAnotherMarket = inAndOutsDto.SpisannyaOnAnotherMarket;
+                            PrihodsGrid.ItemsSource = null;
+                            PrihodsGrid.ItemsSource = Prihods;
+                            RozhodsGrid.ItemsSource = null;
+                            RozhodsGrid.ItemsSource = Rozhods;
+                            LenaSGrid.ItemsSource = null;
+                            LenaSGrid.ItemsSource = LenaS;
+                            LesiaSGrid.ItemsSource = null;
+                            LesiaSGrid.ItemsSource = LesiaS;
+                            SpisannyaGrid.ItemsSource = null;
+                            SpisannyaGrid.ItemsSource = Spisannya;
+                            SpisannyaOnAnotherMarketGrid.ItemsSource = null;
+                            SpisannyaOnAnotherMarketGrid.ItemsSource = SpisannyaOnAnotherMarket;
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string WEBSERVICE_URL1 = StaticHelper.URL + $@"api/Apii/UpdateInAndOuts";
+
+            var webRequest1 = System.Net.WebRequest.Create(WEBSERVICE_URL1);
+            if (webRequest1 != null)
+            {
+                webRequest1.Method = "POST";
+                webRequest1.Timeout = 12000;
+                webRequest1.ContentType = "application/json";
+                webRequest1.Headers.Add("Safety", "Safety");
+                using (var streamWriter2 = new StreamWriter(webRequest1.GetRequestStream()))
+                {
+
+                    InAndOutsDto outsDto = new InAndOutsDto() { LenaS = LenaS, LesiaS = LesiaS, Rozhods = Rozhods, Prihods = Prihods, Spisannya = Spisannya, SpisannyaOnAnotherMarket = SpisannyaOnAnotherMarket };
+                    var json = JsonConvert.SerializeObject(outsDto);
+                    streamWriter2.Write(json);
+                }
+                //webRequest1.Headers.Add("SpecialCode", Code.Text);
+                using (System.IO.Stream s1 = webRequest1.GetResponse().GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr1 = new System.IO.StreamReader(s1))
+                    {
+                        try
+                        {
+                            var jsonResponse1 = sr1.ReadToEnd();
+                            MessageBox.Show("Зберегли!");
+                            Update();
+
+
+
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Error");
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ Magazine/ModelsDto/InAndOutsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.ModelsDto
+{
+    public class InAndOutsDto
+    {
+        public List<SpisannyaOnAnotherMarket> SpisannyaOnAnotherMarket { get; set; }
+        public List<CalcIn> Prihods { get; set; }
+        public List<CalcOut> Rozhods { get; set; }
+        public List<Spisannya> Spisannya { get; set; }
+        public List<Lesia> LesiaS { get; set; }
+        public List<Lena> LenaS { get; set; }
+
+    }
+}
